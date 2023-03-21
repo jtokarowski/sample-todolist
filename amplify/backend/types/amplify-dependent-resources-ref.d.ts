8ee71a444d067f7daf3916d008153913a331ae06
John Tokarowski <Johnt3@bu.edu>
2023-03-21 11:16:01 +0000

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "todolistamplifycfa96284": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
